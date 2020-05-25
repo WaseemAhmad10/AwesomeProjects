acda27ec0f303a5f09264dcd9dae0644c0b7541e
Wasem Ahmad <er.waseem10@gmail.com>
2020-05-25 18:03:27 +0530

    add react native paper

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Appbar.Header>
+        <Appbar.Content title="AwesomeProjects" />
+      </Appbar.Header>
+      <Text>Hello Worlds</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
